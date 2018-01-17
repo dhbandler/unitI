8dd1ce5d126d61519b5128c8a2f81daca3b835cc
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-17 08:18:04 -0500

    Update inputdemo.py

--- inputdemo.py
+++ inputdemo.py
@@ -1,1 +1,9 @@
+#Daniel
+#1/17/18
+#inputdemo.py how to use input
 
+name = input("What is your name? ")
+print("hello", name)
+
+age = int(input("How old are you"? ))
+print("you are", age+1, "years old!")
