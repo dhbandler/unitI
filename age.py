15d3cfb0bfca2c6e32bc3ca6569618a2c08b308b
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-17 08:37:10 -0500

    Updated from Brython Server: 1/17/2018 8:37:09 AM

--- age.py
+++ age.py
@@ -1,1 +1,11 @@
-
+#Daniel 
+#1/17/18
+#age.py asks for first and last name and age
+name=input("What is your name? ")
+name1, name2= name.split()
+let=len(name1)
+print("your first name has", let, "letters")
+nom=len(name2)
+print("your last name has", nom, "letters")
+age=int(input("how old are you?"))
+print("You are", age+1, "years old next year")
