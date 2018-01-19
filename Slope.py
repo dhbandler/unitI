95d5acb8abba9b0630540e8c1a27448755e7dcb2
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-19 08:44:43 -0500

    Updated from Brython Server: 1/19/2018 8:44:42 AM

--- Slope.py
+++ Slope.py
@@ -1,1 +1,14 @@
+#Daniel
+#1/19/18
+#Slope.py Find the slope to a line
 
+x1=float(input("Input first x value "))
+y1=float(input("Input first y value "))
+x2=float(input("Input second x value "))
+y2=float(input("Input second y value "))
+
+
+rise=(y2-y1)
+run=(x2-x1)
+
+print(rise/run)
