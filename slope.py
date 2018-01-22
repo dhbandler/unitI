52872f601071bd867592f545c536f816cd3ed031
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-22 08:24:07 -0500

    Updated from Brython Server: 1/22/2018 8:24:06 AM

--- slope.py
+++ slope.py
@@ -1,1 +1,15 @@
+#Daniel Bandler
+#1/22/18
+#slope.py calculate slope of given points
 
+x1=float(input("Input the first x value here"))
+y1=float(input("Input the first y value here"))
+
+x2=float(input("Input the second x value here"))
+y2=float(input("Input the second y value here"))
+
+
+rise=(y2-y1)
+run=(x2-x1)
+
+print(rise/run)
