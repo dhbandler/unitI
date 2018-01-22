1492ef21d18a2dac80f424dbc2a8f1dc9223ba4f
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-22 08:20:35 -0500

    Update tip.py

--- tip.py
+++ tip.py
@@ -4,4 +4,12 @@
 
 price=float(input("How much did your meal cost?   "))
 
-print("Your tip is", price*.15, "dollars")
+tip=float(input("What is the decimal value of your tip?"))
+if tip<.1:
+    print("You are going to have to do better than that, you dumb cheapskate")
+
+    
+else:
+    print("Your tip is", price*tip, "dollars")
+    print("Thank you for coming here")
+    
