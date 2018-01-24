4cbdc21d8a801366af4330b832431b52d0315327
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-24 08:33:50 -0500

    Updated from Brython Server: 1/24/2018 8:33:49 AM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,12 @@
+#Daniel Bandler
+#1/24/18
+#eggs.py calculates if eggs can fit in carton
+
+number=int(input("input # of eggs here "))
+
+cart=int(input("input # of cartons here "))
+
+space=cart**12
+
+print(space>=number)
 
