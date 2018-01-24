ab7ec9c6e71d43d1c0a7b549bc726a85345f32df
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-24 08:42:35 -0500

    Updated from Brython Server: 1/24/2018 8:42:34 AM

--- change.py
+++ change.py
@@ -1,1 +1,7 @@
-
+#Daniel
+#1/24/18
+#change.py takes in amound of money and outputs necessary smaller denomination
+import(math)
+val=float(input("Input the number in cents that you have"))
+quart=val//25
+print(quart)
