74081a45d0de2ec0b04af47d29f044c278387f4c
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-25 08:21:17 -0500

    Updated from Brython Server: 1/25/2018 8:21:16 AM

--- change.py
+++ change.py
@@ -7,8 +7,17 @@ val=float(input("Input the number in cents that you have"))
 quart=val//25
 rem1=val % 25
 
-dim=rem1//10
-rem2=rem1 % 10
-print("You have", quart, "quarters")
-print(dim)
+dim = rem1//10
+rem2 = rem1 % 10
+
+nick =  rem2//5
+rem3 = rem2 % 5
+
+pen = rem3//1
+
+
+print("You have ", quart, " quarters")
+print("You have ", dim, " dimes")
+print("You have ", nick, " nickles")
+print("You have ", pen, " pennies")
 
