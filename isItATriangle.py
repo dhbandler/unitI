f9f9f3d2e15af846d7dba5b6bb0d38f1011c1120
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-25 08:30:09 -0500

    Updated from Brython Server: 1/25/2018 8:30:09 AM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,10 @@
+#Daniel
+#1/25/18
+SideA=float(input("Input side A please "))
+SideB=float(input("Input side B please "))
+SideC=float(input("Input side C please "))
 
+great=max(SideA, SideB, SideC)
+least=min(SideA, SideB, SideC)
+mid=(SideA+SideB+SideC)-great-least
+print( great > least + mid)
