ac2125df09132d254dda8797c7461af5774eca62
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-25 08:15:16 -0500

    Updated from Brython Server: 1/25/2018 8:15:15 AM

--- warmup2.py
+++ warmup2.py
@@ -2,7 +2,7 @@
 #1/25/18
 #warmup2.py input name, return random number
 
-nom=input("Input your name aqui")
+nom=input("Input your name aqui  ")
 from random import randint
-luck=randint
+luck=randint(-9999,9999)
 print(luck)
