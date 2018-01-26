53acccf35e2ed73195d7b73ecd35a37674127c2e
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-26 07:56:16 -0500

    Updated from Brython Server: 1/26/2018 7:56:15 AM

--- tip.py
+++ tip.py
@@ -4,10 +4,9 @@
 
 price=float(input("How much did your meal cost?   "))
 
-tip=float(input("What is the decimal value of your tip?"))
+tip=float(input("What is the decimal value of your tip? "))
 if tip<.1:
-    print("You are going to have to do better than that, you dumb cheapskate")
-
+    print("Only a", price*tip, "dollar tip? You are going to have to do better than that, you stingy reprobate")
     
 else:
     print("Your tip is", price*tip, "dollars")
