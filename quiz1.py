51c890ece48004a935129be21df933a77345c650
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-26 08:16:50 -0500

    Updated from Brython Server: 1/26/2018 8:16:50 AM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,15 @@
+#Daniel
+#1/26/18
+#quiz1.py Is the first quiz we have on unit 1
 
+print("Hello!")
+
+name=input("What is your name? ")
+
+num=float(input("What is your favorite number? "))
+
+print(name*10)
+
+num2=float(input("What is your second favorite number "))
+
+print("Your lucky number is... ", num**num2)
