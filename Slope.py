9ed2a0d55bd90c02e63a938996030c643d92aff4
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-02 08:47:28 -0500

    Updated from Brython Server: 2/2/2018 8:47:27 AM

--- Slope.py
+++ Slope.py
@@ -7,10 +7,11 @@ y1=float(input("Input first y value "))
 x2=float(input("Input second x value "))
 y2=float(input("Input second y value "))
 
-
-rise=(y2-y1)
-run=(x2-x1)
-
-slope=rise/run
-print("The slope is", slope)
-print("An equation for the line could be  ", slope, "x+", slope)
+if x1==x2:
+    print("The slope is undefined")
+else:
+    rise=(y2-y1)
+    run=(x2-x1)
+    slope=rise/run
+    print("The slope is", slope)
+    print("An equation for the line could be  ", slope, "x+", slope)
